6474fbbc26d7bfca438e6cd3531c788ea396c4b4
SharonHenning <72208574+SharonHenning@users.noreply.github.com>
2022-11-26 16:20:40 +0000

    feat: create @BeforeEach test colour

--- src/test/java/cs3318/a03/ColourTest.java
+++ src/test/java/cs3318/a03/ColourTest.java
@@ -1,7 +1,17 @@
 package cs3318.a03;
 
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class ColourTest {
 
+    Colour testColour;
+
+    @BeforeEach
+    void createTestColour() {
+        testColour = new Colour();
+    }
 }
