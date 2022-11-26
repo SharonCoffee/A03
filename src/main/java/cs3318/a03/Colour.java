d98ef67005c9df556b4bca1807035ceda293de8a
SharonHenning <72208574+SharonHenning@users.noreply.github.com>
2022-11-26 16:26:07 +0000

    feat: create a test to test the direct inputs and outputs of a test colour object feat: added variables, constructor, getters and setters in Colour class

--- src/main/java/cs3318/a03/Colour.java
+++ src/main/java/cs3318/a03/Colour.java
@@ -1,4 +1,38 @@
 package cs3318.a03;
 
 public class Colour {
+    public float red;
+    public float green;
+    public float blue;
+
+    public Colour(float red, float green, float blue) {
+        this.red = red;
+        this.green = green;
+        this.blue = blue;
+    }
+
+    public void setRed(int red) {
+        this.red = red;
+    }
+
+    public void setGreen(int green) {
+        this.green = green;
+    }
+
+    public void setBlue(int blue) {
+        this.blue = blue;
+    }
+
+    public float getRed() {
+        return red;
+    }
+
+    public float getGreen() {
+        return green;
+    }
+
+    public float getBlue() {
+        return blue;
+    }
+
 }
--- src/test/java/cs3318/a03/ColourTest.java
+++ src/test/java/cs3318/a03/ColourTest.java
@@ -12,6 +12,18 @@ class ColourTest {
 
     @BeforeEach
     void createTestColour() {
-        testColour = new Colour();
+        //testColour = new Colour();
     }
+
+    @Test
+    @DisplayName("Test the direct inputs and outputs of a test colour object")
+    void testDirectInputsAndOutputs() {
+        testColour = new Colour(0.5f, 0.5f, 0.5f);
+        assertEquals(0.5f, testColour.getRed(), 0.1f);
+        assertEquals(0.5f, testColour.getGreen(), 0.1f);
+        assertEquals(0.5f, testColour.getBlue(), 0.1f);
+    }
+
+
+
 }
